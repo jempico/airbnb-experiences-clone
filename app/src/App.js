1e9809e606bba08686395574a53e3c8f7dfe4394
Jemimah <jpfilarca@gmail.com>
2021-11-24 23:20:20 +0100

    modified app.js

--- app/src/App.js
+++ app/src/App.js
@@ -6,12 +6,13 @@ import Card from "./components/Card"
 import "./style.css";
 
 export default function App({experiences}) {
+    const experiencesElements = experiences.map( (experience) => (<Card key={experience.id} {...experience}/> ))
     return (
         <div className="app_container">
             <Navbar/>
             <Hero/>
             <section className="card_container">
-                {experiences.map( (experience) => (<Card key={experience.id} {...experience}/> ))}
+                {experiencesElements}
             </section>
             
         </div>
