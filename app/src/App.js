7753a03cf3ed4ba74cd2993518f7efcf6ca8ca92
Jemimah <jpfilarca@gmail.com>
2021-11-24 13:21:54 +0100

    fixed App component

--- app/src/App.js
+++ app/src/App.js
@@ -2,18 +2,17 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-import {Experiences} from "./dummyData.js";
 
 import "./style.css";
 
-export default function App() {
+export default function App({experiences}) {
     return (
         <div className="app_container">
             <Navbar/>
             <Hero/>
             <section className="card_container">
-                {Experiences.map( (experience) => (<Card key={experience.id} {...experience}/> ))}
-        </section>
+                {experiences.map( (experience) => (<Card key={experience.id} {...experience}/> ))}
+            </section>
             
         </div>
     )
